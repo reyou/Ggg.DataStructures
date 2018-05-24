0c5f9110d70f40629440eb857ae194c54b6b654e
Teoman Shipahi <aytekoz@yahoo.com>
2018-05-23 23:25:58 -0400

    deleted non sense projects.

--- apps/app-cplusplus/GggDataStructuresCpp/GggDataStructuresMain.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class GggDataStructuresMain
-{
-public:
-	GggDataStructuresMain();
-	~GggDataStructuresMain();
-};
-
--- apps/app-javascript/GggFindTheMissingNumber.js
+++ apps/app-javascript/GggFindTheMissingNumber.js
@@ -2,34 +2,14 @@
 // https://www.youtube.com/watch?v=lBk6fVhuCyw
 // http://www.geeksforgeeks.org/find-the-missing-number/
 function getMissingNo(a, n) {
-    console.log("var i = 0;");
-    console.log("var " + i + " = 0;");
-    console.log("\n");
-    var i = 0;
-    console.log("var total = (n + 1) * (n + 2) / 2;");
-    console.log("var " + total + " = (" + n + " + 1) * (" + n + " + 2) / 2;");
-    console.log("\n");
-    var total = (n + 1) * (n + 2) / 2;
-    console.log(" for (i = 0; i < n; i++) {");
-    console.log(" for (" + i + " = 0; " + i + " < " + n + "; " + i + "++) {");
-    console.log("\n");
-    for (i = 0; i < n; i++) {
-        console.log("a: ", a);
-        console.log("/n");
-        console.log("total -= a[i];");
-        console.log("" + total + " -= a[" + i + "];");
-        console.log("\n");
-        total -= a[i];
-        console.log("total -= a[i];");
-        console.log("" + total + " -= a[" + i + "];");
-        console.log("\n");
-    }
-    console.log("return total;");
-    console.log("return " + total + ";");
-    console.log("\n");
-    return total;
+  var i = 0;
+  var total = (n + 1) * (n + 2) / 2;
+  for (i = 0; i < n; i++) {
+    total -= a[i];
+  }
+  return total;
 }
 
-var a = [1, 2, 4, 5, 6]
+var a = [1, 2, 4, 5, 6];
 var miss = getMissingNo(a, a.length);
 console.log(miss);

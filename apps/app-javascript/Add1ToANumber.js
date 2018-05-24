d00350794a5d29f04d23f0da57ee6384f3aa4d94
Teoman Shipahi <aytekoz@yahoo.com>
2018-05-23 23:55:30 -0400

    Add1ToANumber.js

--- apps/app-javascript/Add1ToANumber.js
+++ apps/app-javascript/Add1ToANumber.js
@@ -4,53 +4,52 @@
 /*Number is represented in linked list such that each digit corresponds 
 to a node in linked list. Add 1 to it. For example 1999 is represented 
 as (1-> 9-> 9 -> 9) and adding 1 to it should change it to (2->0->0->0)*/
-var Node = function (data) {
-    this.data = data;
-    this.next = null;
-    this.prev = null;
-}
-var AddOneFun = function (node) {
-    // find tail
-    var tail;
-    tail = node.next;
-    while (tail != null) {
-        if (tail.next == null) {
-            break;
-        }
-        tail = tail.next;
+var Node = function(data) {
+  this.data = data;
+  this.next = null;
+  this.prev = null;
+};
+var AddOneFun = function(node) {
+  // find tail
+  var tail;
+  tail = node.next;
+  while (tail != null) {
+    if (tail.next == null) {
+      break;
     }
+    tail = tail.next;
+  }
 
-    var keepAdding = true;
-    var newHead;
-    while (keepAdding) {
-        tail.data = tail.data + 1;
-        if (tail.data == 10) {
-            tail.data = 0;
-            if (tail.prev == null) {
-                newHead = new Node(tail.data);
-                newHead.next = tail;
-                tail.prev = newHead;
-                keepAdding = false;
-            }
-            else {
-                tail = tail.prev;                
-            }
-
-        }
-        else {
-            keepAdding = false;
-        }
+  var keepAdding = true;
+  var newHead;
+  while (keepAdding) {
+    tail.data = tail.data + 1;
+    if (tail.data == 10) {
+      tail.data = 0;
+      if (tail.prev == null) {
+        newHead = new Node(1);
+        newHead.next = tail;
+        tail.prev = newHead;
+        keepAdding = false;
+      } else {
+        tail = tail.prev;
+      }
+    } else {
+      keepAdding = false;
     }
-    console.log(node.data);
-    var next = node.next;
-    while (next != null) {
-        console.log(next.data);
-        next = next.next;
-    }
-}
+  }
+  var next = node;
+  if (newHead) {
+    next = newHead;
+  }
+  while (next != null) {
+    console.log(next.data);
+    next = next.next;
+  }
+};
 var node1 = new Node(1);
-var node2 = new Node(9);
-var node3 = new Node(9);
+var node2 = new Node(0);
+var node3 = new Node(0);
 var node4 = new Node(9);
 node1.next = node2;
 node2.next = node3;
@@ -58,4 +57,13 @@ node3.next = node4;
 node4.prev = node3;
 node3.prev = node2;
 node2.prev = node1;
+printList(node1);
+console.log("Solution=>");
 AddOneFun(node1);
+
+function printList(node) {
+  while (node != null) {
+    console.log(node.data);
+    node = node.next;
+  }
+}

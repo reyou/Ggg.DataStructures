cf1e6097b229cd23a33efa7fb45414e7367c199f
Teoman Shipahi <aytekoz@yahoo.com>
2019-01-21 09:10:12 -0500

    permute

--- apps/app-web/geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/intro4backtrackingTest/index.js
+++ apps/app-web/geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/intro4backtrackingTest/index.js
@@ -1,1 +1,22 @@
-// TODO
+/*
+abc   a (bc)   ab,ac   abc,acb
+      b (ac)   ba,bc   bac,bca
+      c (ab)  ca,cb   cab,cba
+*/
+
+function permute(text) {
+  let left = text;
+  let right = "";
+  explore(text, left, right);
+}
+function explore(text, left, right) {
+  if (right.length >= text.length) {
+    console.log(right);
+  } else {
+    for (let i = 0; i < left.length; i++) {
+      let remaining = left.substring(0, i) + left.substring(i + 1);
+      explore(text, remaining, right + left[i]);
+    }
+  }
+}
+permute("abc");

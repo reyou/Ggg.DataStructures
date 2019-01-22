dbbc2b6c223f11eee79159fd60f691d0b3eb67a3
aozdemir <aozdemir@webmd.net>
2019-01-22 13:05:44 -0500

    subsetprint

--- apps/app-web/see.stanford.edu/materials/icspacs106b/H19-RecBacktrackExamples.pdf/intro1SubsetsSelf/index.js
+++ apps/app-web/see.stanford.edu/materials/icspacs106b/H19-RecBacktrackExamples.pdf/intro1SubsetsSelf/index.js
@@ -1,9 +1,27 @@
-/* 
+/*
 abc
-ab
-ac
-a
-bc
-b
-c 
+    {}
+    a
+        ab
+            abc
+        ac
+    b
+        bc
+    c
 */
+
+function subsetprint(text) {
+    let builder = "";
+    subset(builder, text)
+}
+function subset(builder, rem) {
+    console.log("subset: " + builder);
+    if (rem.length == 0) {
+        return;
+    }
+    for (let i = 0; i < rem.length; i++) {
+        subset(builder + rem[i], rem.substring(i + 1));
+    }
+}
+subsetprint("betul")
+
--- apps/app-web/see.stanford.edu/materials/icspacs106b/H19-RecBacktrackExamples.pdf/intro2SubsetsBook/index.js
+++ apps/app-web/see.stanford.edu/materials/icspacs106b/H19-RecBacktrackExamples.pdf/intro2SubsetsBook/index.js
@@ -10,13 +10,18 @@ c
 function subsets(text) {
   resolve("", text);
 }
+let depth = "-";
 function resolve(soFar, rest) {
+  console.log("incoming:", soFar, rest);
   if (rest == "") {
-    console.log(soFar);
+    console.log(depth + soFar);
   } else {
+    depth = depth + "-"
     resolve(soFar + rest[0], rest.substring(1));
+    depth = depth + "-"
     resolve(soFar, rest.substring(1));
   }
+  depth = depth.substring(1)
 }
 
 subsets("abc");

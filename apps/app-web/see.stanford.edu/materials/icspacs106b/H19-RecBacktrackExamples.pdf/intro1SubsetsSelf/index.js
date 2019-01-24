fa9476505454000884631e6a583d0bc49f32fea4
Teoman Shipahi <aytekoz@yahoo.com>
2019-01-24 00:30:19 -0500

    FindWord2

--- apps/app-web/see.stanford.edu/materials/icspacs106b/H19-RecBacktrackExamples.pdf/intro1SubsetsSelf/index.js
+++ apps/app-web/see.stanford.edu/materials/icspacs106b/H19-RecBacktrackExamples.pdf/intro1SubsetsSelf/index.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-function FindWord(text) {
-  let builder = "";
-  let remaining = text;
-  let result = FindWord2(builder, remaining);
-  return result;
-}
-function CheckIfExists(text) {
-  if (text.indexOf("car") > -1) {
-    console.log("Text found in", text);
-    return true;
-  }
-  return false;
-}
-function FindWord2(builder, remaining) {
-  if (remaining.length == 0) {
-    let result = CheckIfExists(builder);
-    return result;
-  } else {
-    for (let i = 0; i < remaining.length; i++) {
-      let rem = remaining.substring(0, i) + remaining.substring(i + 1);
-      let result = FindWord2(builder + remaining[i], rem);
-      if (result) {
-        return result;
-      }
-    }
-  }
-}
-
-let result = FindWord("cxrza");
-console.log(result);
-=======
 /*
 abc
     {}
@@ -43,18 +11,16 @@ abc
 */
 
 function subsetprint(text) {
-    let builder = "";
-    subset(builder, text)
+  let builder = "";
+  subset(builder, text);
 }
 function subset(builder, rem) {
-    console.log("subset: " + builder);
-    if (rem.length == 0) {
-        return;
-    }
-    for (let i = 0; i < rem.length; i++) {
-        subset(builder + rem[i], rem.substring(i + 1));
-    }
+  console.log("subset: " + builder);
+  if (rem.length == 0) {
+    return;
+  }
+  for (let i = 0; i < rem.length; i++) {
+    subset(builder + rem[i], rem.substring(i + 1));
+  }
 }
-subsetprint("abc")
-
->>>>>>> a65b5b59e7409201ff0bd187eebce86b2d620d96
+subsetprint("abc");
--- apps/app-web/see.stanford.edu/materials/icspacs106b/H19-RecBacktrackExamples.pdf/intro2SubsetsBook/index.js
+++ apps/app-web/see.stanford.edu/materials/icspacs106b/H19-RecBacktrackExamples.pdf/intro2SubsetsBook/index.js
@@ -10,18 +10,15 @@ c
 function subsets(text) {
   resolve("", text);
 }
-let depth = "-";
+
 function resolve(soFar, rest) {
-  console.log("incoming:", soFar, rest);
   if (rest == "") {
-    console.log(depth + soFar);
+    console.log(soFar);
   } else {
-    depth = depth + "-"
     resolve(soFar + rest[0], rest.substring(1));
-    depth = depth + "-"
+
     resolve(soFar, rest.substring(1));
   }
-  depth = depth.substring(1)
 }
 
 subsets("abc");

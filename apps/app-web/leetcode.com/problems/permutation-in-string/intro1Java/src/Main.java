cdfedc19fbbb2c177c706f73945c039632fb5ecc
Teoman Shipahi <aytekoz@yahoo.com>
2019-01-27 15:58:55 -0500

    checkInclusion

--- apps/app-web/leetcode.com/problems/permutation-in-string/intro1Java/src/Main.java
+++ apps/app-web/leetcode.com/problems/permutation-in-string/intro1Java/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        boolean checkInclusion = sol.checkInclusion("abc", "xyzbcark");
+        boolean checkInclusion = sol.checkInclusion("abc", "tttxayzbcark");
         System.out.println("checkInclusion: " + checkInclusion);
 
     }
--- apps/app-web/leetcode.com/problems/permutation-in-string/intro1Java/src/Solution.java
+++ apps/app-web/leetcode.com/problems/permutation-in-string/intro1Java/src/Solution.java
@@ -4,16 +4,20 @@ class Solution {
         for (char ch: s1.toCharArray()) {
             map[ch]++;
         }
-        for (int idx = 0, start = 0; idx < s2.length(); idx++) {
-            char ch = s2.charAt(idx);
-            if (--map[ch] < 0)
-                while (map[ch] != 0) {
-                    map[s2.charAt(start++)]++;
+        for (int pointer = 0, matchStart = 0; pointer < s2.length(); pointer++) {
+            char s2CharMember = s2.charAt(pointer);
+            // s2 char member not found in s1
+            map[s2CharMember]--;
+            if (map[s2CharMember] < 0) {
+                while (map[s2CharMember] != 0) {
+                    char c = s2.charAt(matchStart++);
+                    map[c]++;
                 }
-            else if (idx - start + 1 == s1.length()) {
-                return true;
             }
-        }
+            // s2 char member found in
+            else if (pointer - matchStart + 1 == s1.length()) {
+                return true;
+            }        }
         return false;
     }
 }

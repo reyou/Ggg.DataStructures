b3b498d4915829fee85428de378c0fe97a8c7726
Teoman Shipahi <aytekoz@yahoo.com>
2019-01-27 02:24:51 -0500

    checkInclusion

---
+++ apps/app-web/leetcode.com/problems/permutation-in-string/intro1Java/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        boolean checkInclusion = sol.checkInclusion("abc", "xyzbcark");
+        System.out.println("checkInclusion: " + checkInclusion);
+
+    }
+}
--- apps/app-web/leetcode.com/problems/permutation-in-string/intro1Java/src/Solution.java
+++ apps/app-web/leetcode.com/problems/permutation-in-string/intro1Java/src/Solution.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
-public class Solution {
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] map = new int[256];
+        for (char ch: s1.toCharArray()) {
+            map[ch]++;
+        }
+        for (int idx = 0, start = 0; idx < s2.length(); idx++) {
+            char ch = s2.charAt(idx);
+            if (--map[ch] < 0)
+                while (map[ch] != 0) {
+                    map[s2.charAt(start++)]++;
+                }
+            else if (idx - start + 1 == s1.length()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }

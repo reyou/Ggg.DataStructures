dc03123557dabeb6791a2d789227b1b0383cc359
Teoman Shipahi <aytekoz@yahoo.com>
2019-03-04 23:02:34 -0500

    Coding Interview Question: Tower Hopper Problem

--- apps/app-web/youtube.com/Facebook-Coding-Interview-Question/intro.js
+++ apps/app-web/youtube.com/Facebook-Coding-Interview-Question/intro.js
@@ -1,18 +1,25 @@
 function resolve(data) {
-  let builder = "";
-  let rem = data;
-  return resolve2(data, builder, rem, []);
-}
-function resolve2(data, builder, rem) {
-  console.log(builder);
-  if (rem.length == 0) {
-    return;
-  }
-  for (let i = 0; i < rem.length; i++) {
-    let builder2 = builder + rem[i];
-    let rem2 = rem.substring(i + 1);
-    resolve2(data, builder2, rem2);
-  }
+  return resolve2(data);
 }
 
-console.log(resolve("1234"));
+function resolve2(data) {
+  if (data.length == "0") {
+    return 1;
+  }
+  let data1 = data.substring(0, 1);
+  if (data1 == "0") {
+    return 0;
+  }
+  let remaining = data.substring(1);
+  let result = resolve2(remaining);
+  if (data.length >= 2) {
+    let pick2 = data.substring(0, 2);
+    if (parseInt(pick2) <= 26) {
+      let remaining2 = data.substring(2);
+      result += resolve2(remaining2);
+    }
+  }
+  return result;
+}
+
+console.log(resolve("12345"));

c5b7807681f19ad82d44783ec7f2792eb12a02e2
Teoman Shipahi <aytekoz@yahoo.com>
2019-05-01 00:06:50 -0400

    Two Sum II - Input array is sorted

--- apps/app-web/leetcode.com/explore/learn/card/binary-search/144/more-practices/1031/intro1.js
+++ apps/app-web/leetcode.com/explore/learn/card/binary-search/144/more-practices/1031/intro1.js
@@ -3,31 +3,17 @@
  * @return {number}
  */
 var findMin = function(nums) {
-  if (nums.length == 1) return nums[0];
   let left = 0;
   let right = nums.length - 1;
-  while (left <= right) {
+  while (left < right) {
     let mid = left + Math.floor((right - left) / 2);
-    if (nums[mid] < nums[mid - 1]) {
-      return nums[mid];
-    }
-    if (nums[mid] > nums[mid + 1]) {
-      return nums[mid + 1];
-    }
-    // go left
-    if (nums[0] <= nums[right]) {
-      right = mid - 1;
-    }
-    // go right
-    else {
+    if (nums[mid] > nums[right]) {
       left = mid + 1;
+    } else if (nums[mid] < nums[right]) {
+      right = mid;
+    } else {
+      right--;
     }
-  }
-  if (right < 0) {
-    return nums[left];
-  }
-  if (left > right) {
-    return nums[right];
   }
   return nums[left];
 };

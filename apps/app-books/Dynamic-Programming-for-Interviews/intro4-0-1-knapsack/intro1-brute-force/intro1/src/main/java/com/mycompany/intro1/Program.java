7cac2d3c48709619c3581221f9f335f895a85cf0
Teoman Shipahi <aytekoz@yahoo.com>
2019-06-25 00:11:30 -0400

    knapsack

---
+++ apps/app-books/Dynamic-Programming-for-Interviews/intro4-0-1-knapsack/intro1-brute-force/intro1/src/main/java/com/mycompany/intro1/Program.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.intro1;
+
+/**
+ *
+ * @author dj_re
+ * \intro4-0-1-knapsack\intro1-brute-force
+ */
+public class Program {
+       public static void main(String[] args) {
+        Program program = new Program();
+      
+        System.out.println("knapsack result: ");
+    }
+}

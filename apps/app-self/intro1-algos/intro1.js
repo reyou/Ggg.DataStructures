7a25346748d1d12dd66016e6035bae6395143941
Teoman Shipahi <aytekoz@yahoo.com>
2021-04-18 18:05:03 -0500

    factorial

--- apps/app-self/intro1-algos/intro1.js
+++ apps/app-self/intro1-algos/intro1.js
@@ -1,2 +1,10 @@
-console.log('hello');
-console.log('hello');
+function factorial(number) {
+  if (number === 1) {
+    console.log(`Ending recursive: ${number}`);
+    return number;
+  }
+  console.log(`Calculating: factorial(${number} - 1) * ${number}`);
+  return factorial(number - 1) * number;
+}
+
+console.log(factorial(5));

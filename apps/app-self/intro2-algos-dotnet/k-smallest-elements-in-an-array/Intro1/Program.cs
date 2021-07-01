f2dc43fc67bb4ed03a33c20afccde3689e4595fb
Teoman Shipahi <aytekoz@yahoo.com>
2021-06-30 23:42:55 -0500

    ds pq

---
+++ apps/app-self/intro2-algos-dotnet/k-smallest-elements-in-an-array/Intro1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Intro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = new[] { -1, 2, 4, 21, -43, 7, 9, 65, 24 };
+            int k = 3;
+            int[] result = KSmallestElements(elements, k);
+            Console.WriteLine(result);
+        }
+
+        private static int[] KSmallestElements(int[] elements, int k)
+        {
+            return elements;
+        }
+    }
+}
